0d005260e3bb2647d6a78f79687d550359101096
Majid Ghafoorzade <m_ghafoorzadeh@namava.local>
2022-12-27 15:07:22 +0330

    updates

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -1,12 +1,9 @@
-import React from "react";
+import React, { FC } from "react";
+import { ButtonProps } from "./Button.interface";
 import styles from "./Button.module.css";
 
-export interface ButtonProps {
-  label: string;
-}
-
-const Button = (props: ButtonProps) => {
-  return <button className={styles.Button}>{props.label}</button>;
+const Button: FC<ButtonProps> = ({ label }) => {
+  return <button className={styles.Button}>{label}</button>;
 };
 
 export default Button;
