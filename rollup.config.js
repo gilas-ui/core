c473d229507484899271791e1c46ebc801532eb7
Majid Ghafoorzade <m_ghafoorzadeh@namava.local>
2022-12-27 13:18:51 +0330

    updates

--- rollup.config.js
+++ rollup.config.js
@@ -2,9 +2,10 @@ import resolve from "@rollup/plugin-node-resolve";
 import commonjs from "@rollup/plugin-commonjs";
 import typescript from "@rollup/plugin-typescript";
 import dts from "rollup-plugin-dts";
-
-// const packageJson = require("./package.json");
+import postcss from "rollup-plugin-postcss";
 import packageJson from "./package.json" assert { type: "json" };
+import terser from '@rollup/plugin-terser';
+import peerDepsExternal from 'rollup-plugin-peer-deps-external';
 
 export default [
   {
@@ -22,14 +23,18 @@ export default [
       },
     ],
     plugins: [
+      peerDepsExternal(),
       resolve(),
       commonjs(),
       typescript({ tsconfig: "./tsconfig.json" }),
+      postcss(),
+      terser(),
     ],
   },
   {
     input: "dist/esm/types/index.d.ts",
     output: [{ file: "dist/index.d.ts", format: "esm" }],
     plugins: [dts()],
+    external: [/\.css$/],
   },
 ];
